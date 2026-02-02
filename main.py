542550e7fe60b1e19275dcc2d5bda3e9eb40d255
Yannik Höreth <y_hoereth16@informatik.uni-kl.de>
2026-02-02 09:20:19 +0100

    implemented prompts

--- main.py
+++ main.py
@@ -9,29 +9,13 @@ logfire.configure()
 logfire.instrument_pydantic_ai()
 
 system_prompt = load_prompt("prompts/system.md")
+task_prompt = load_prompt("prompts/what2eat/task.md")
 
 agent = Agent(
     "google-gla:gemini-2.5-pro",
     instructions=system_prompt,
 )
 
-result = agent.run_sync(
-    """
-    def add_numbers(a: int, b: int) -> int:
-    
-    "
-        Add two integers and return their sum.
+result = agent.run_sync(task_prompt)
 
-        Args:
-            a (int): The first integer.
-            b (int): The second integer.
-
-        Returns:
-            int: The sum of a and b.
-
-        Raises:
-            TypeError: If either argument is not an integer.
-    "
-    """
-)
 print(result.output)
