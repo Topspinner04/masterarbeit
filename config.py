f4ecac434c9c3620dae311784deaf4ed38aa5e6d
Yannik Höreth <y_hoereth16@informatik.uni-kl.de>
2026-02-19 16:43:01 +0100

    edit ref path

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 PROJECT = "what2eat"
 TREATMENT = "treatment_a"  # or "treatment_b"
-REF_PATH = f"ref/{PROJECT}"
+REF_PATH = (
+    f"ref/{PROJECT}/{PROJECT}"  # tool can only access source code for now and not docs
+)
 GENERATED_PATH = f"generated/{PROJECT}/{TREATMENT}"
 PROMPT_PATH = f"prompts/{PROJECT}/{TREATMENT}"
